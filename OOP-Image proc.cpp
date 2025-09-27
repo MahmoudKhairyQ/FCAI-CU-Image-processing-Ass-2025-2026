840132ea62bb17fe984a34fbf1a8c159264f113b
Ahmed Abdullah <bn36203@gmail.com>
2025-09-28 01:02:05 +0300

    Update OOP-Image proc.cpp

--- OOP-Image proc.cpp
+++ OOP-Image proc.cpp
@@ -30,7 +30,37 @@ int menu(string picname){
 
 
         else if(filtersnum == 1) {                  //Grayscale conv
-        
+            string filename;
+    cout << "Pls enter colored image name to turn to gray scale: ";
+    cin >> filename;
+
+    Image image(filename);
+
+    for (int i = 0; i < image.width; ++i)
+    {
+        for (int j = 0; j < image.height; ++j)
+        {
+            unsigned int avg = 0; 
+
+            for (int k = 0; k < 3; ++k)
+            {
+                avg += image(i, j, k);
+            }
+
+            avg /= 3; 
+            image(i, j, 0) = avg;
+            image(i, j, 1) = avg;
+            image(i, j, 2) = avg;
+        }
+    }
+
+    cout << "Pls enter image name to store new image\n";
+    cout << "and specify extension .jpg, .bmp, .png, .tga: ";
+
+    cin >> filename;
+    image.saveImage(filename);
+
+    return 0;
         }
 
 
@@ -172,6 +202,7 @@ int main (){
 
     return 0;
 }
+
 
 
 
