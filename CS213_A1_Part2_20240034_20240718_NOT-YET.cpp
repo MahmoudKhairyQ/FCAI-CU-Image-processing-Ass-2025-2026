bbad0f511e6060ed175c62e7ac4be673d037c9dc
Ahmed Abdullah <bn36203@gmail.com>
2025-10-07 19:21:24 +0300

    Update CS213_A1_Part2_20240034_20240718_NOT-YET.cpp

--- CS213_A1_Part2_20240034_20240718_NOT-YET.cpp
+++ CS213_A1_Part2_20240034_20240718_NOT-YET.cpp
@@ -39,38 +39,31 @@ int menu(string picname){
         }
 
 
-        else if(filtersnum == 1) {                  //Grayscale conv
-            string filename;
-    cout << "Pls enter colored image name to turn to gray scale: ";
-    cin >> filename;
-
-    Image image(filename);
+ else if(filtersnum == 1) {                  //Grayscale conv
+            string imageName;
+    cout << "Enter the colored image name to convert it to grayscale: ";
+    cin >> imageName;
+    Image image(imageName);
 
     for (int i = 0; i < image.width; ++i)
     {
         for (int j = 0; j < image.height; ++j)
         {
             unsigned int avg = 0; 
-
             for (int k = 0; k < 3; ++k)
             {
                 avg += image(i, j, k);
             }
-
             avg /= 3; 
             image(i, j, 0) = avg;
             image(i, j, 1) = avg;
             image(i, j, 2) = avg;
         }
     }
-
-    cout << "Pls enter image name to store new image\n";
-    cout << "and specify extension .jpg, .bmp, .png, .tga: ";
-
-    cin >> filename;
-    image.saveImage(filename);
-
-    return 0;
+    cout << "Enter the name of the new image (include extension .jpg, .bmp, .png, or .tga): ";
+    cin >> imageName;
+    image.saveImage(imageName);
+    
         }
 
 
@@ -549,6 +542,7 @@ int main (){
 
     return 0;
 }
+
 
 
 
